17629d7bbf7858fba37dfdc839080610daea1132
Shiny Saing <shiny.saing@pm.me>
2022-10-26 21:39:00 +0200

    Remove mock data manager (unused)

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,6 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App';
 import i18n from './services/i18n/i18n';
 import DataManagerProvider from './services/dataManager/DataManagerProvider';
-import { MockDataManager } from './services/dataManager/MockDataManager';
 import { DefaultDataManager } from './services/dataManager/DefaultDataManager';
 
 import './antd.less';
@@ -14,10 +13,7 @@ import './antd.less';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-
-// const dataManager = new MockDataManager();
 const dataManager = new DefaultDataManager('https://kiken-qr.com');
-
 const queryClient = new QueryClient();
 
 root.render(
